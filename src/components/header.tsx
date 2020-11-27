e6dc53e3fa14b3d89c3f9c969f92244d011e61d3
DmitrySimakovich <sim.d.a.work@gmail.com>
2020-11-27 15:28:06 +0300

    init commit

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import style from './header.module.css';
+
+type propsType = {}
+
+const Header: FC<propsType> = () => {
+    return <>
+        <div className={style.header}>
+
+        </div>
+    </>
+}
+export default Header
